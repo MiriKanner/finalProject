e173d73312e8ec7adcc9d99701ed6ef6afcc2f52
ayalaSch <ayalayaffa@gmail.com>
2024-06-22 23:44:34 +0300

    basic for adding child function

---
+++ Client/src/componnents/Children/AddChild.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function AddChild(props) {
+    return(<><div>here will go the proccess of adding child</div></>)
+}
+
+export default AddChild;
--- Client/src/componnents/Children/MyChildren.jsx
+++ Client/src/componnents/Children/MyChildren.jsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
-import {getReq} from "../../serverquests";
+import { getReq } from "../../serverquests";
 import { useForm } from "react-hook-form";
 import { Link, json, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
-
+import AddChild from "./AddChild";
 
 
 function MyChildren() {
@@ -13,14 +13,14 @@ function MyChildren() {
     const user = useContext(UserContext).user;
     const { register, handleSubmit } = useForm();
     const [children, setChildren] = useState([])
-    const [displayAddMyChildrenAlbum, setDisplayAddMyChildrenAlbum] = useState(false);
+    const [displayAddChild, setDisplayAddChild] = useState(false)
 
     useEffect(() => {
         const req = {
             method: "GET",
             route: `children/myChildren/${user.username}`
         };
-        getReq(req).then((response)=>response.json()).then((responseJson) => {
+        getReq(req).then((response) => response.json()).then((responseJson) => {
             setChildren(responseJson)
             //console.log(responseJson);
         }).catch(err => { })
@@ -29,14 +29,14 @@ function MyChildren() {
 
     return (
         <>
-            {/* <button onClick={() => setDisplayAddMyChildrenAlbum(!displayAddMyChildrenAlbum)}>Add Album to my child</button> */}
-            {/* {displayAddMyChildrenAlbum && <AddMyChildrenAlbum />} */}
+            <button onClick={() => setDisplayAddChild(!displayAddChild)}>Add Child</button>
+            {displayAddChild && <AddChild setDisplayAddChild={setDisplayAddChild} />}
             <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
                 {children.map((child, index) => (
                     <div key={index}>
                         <Card // onClick={()=> navigate(`./${child.albumId}`)}
                             shadow="sm" key={child.id} isPressable
-                            onClick={()=>navigate(`./${child.childName}`)} >
+                            onClick={() => navigate(`./${child.childName}`)} >
                             {/* <CardBody className="overflow-visible p-0">
                                 <Image
                                     shadow="sm"
@@ -63,6 +63,5 @@ function MyChildren() {
         </>
     );
 }
-
 
 export default MyChildren;
